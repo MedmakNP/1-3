58df3b5e139e10303834e2524bcfbbd2d97889fe
Nazar <95432399+MedmakNP@users.noreply.github.com>
2022-06-04 19:41:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+let usersTable = document.getElementById('usersTable');
+function DataTable(config, data) {
+    let table = document.createElement('table');
+    table.classList.add('table')
+    let tHead = document.createElement('thead')
+    let tr = document.createElement('tr');
+  
+    table.appendChild(tHead);
+    tHead.appendChild(tr);
+
+    for(i = 0;i<config.columns.length; i++)
+    {
+        let th = document.createElement('th')
+        th.innerHTML = config.columns[i].title;
+        th.classList.add('Th')
+        tr.appendChild(th)
+    }
+    let tbody = document.createElement('tbody');
+
+    table.insertAdjacentElement('beforeend', tbody);
+    for(i = 0;i<data.length;i++)
+    {
+        let row = document.createElement('tr')
+        for(j = 0;j < Object.keys(data[i]).length;j++)
+        {
+            let td = document.createElement('td')
+            td.innerHTML = Object.values(data[i])[j]
+            row.appendChild(td)
+            td.classList.add('td')
+        }
+        tbody.appendChild(row)
+    }
+    usersTable.appendChild(table)
+ }
+ 
+ const config1 = {
+   parent: '#usersTable',
+   columns: [
+     {title: '№', value: 'id'},      
+     {title: 'Имя', value: 'name'},
+     {title: 'Фамилия', value: 'surname'},
+     {title: 'Возраст', value: 'age'},
+   ]
+ };
+ 
+ const users = [
+   {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
+   {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
+ ];
+ 
+ DataTable(config1, users);
+ 
